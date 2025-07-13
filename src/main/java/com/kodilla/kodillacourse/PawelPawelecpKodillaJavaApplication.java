60a67b43261e333fc8ead9a5cf939da68b26b4ac
pawel-pp <sub@pawelecp.pl>
2025-07-13 14:53:58 +0200

    first commit

---
+++ src/main/java/com/kodilla/kodillacourse/PawelPawelecpKodillaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillacourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawelPawelecpKodillaJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PawelPawelecpKodillaJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/kodillacourse/PawelPawelecpKodillaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillacourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawelPawelecpKodillaJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
